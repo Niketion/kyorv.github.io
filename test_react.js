b6e90f276d2c05f0605d1bb4908b40e9f54c4e69
Nic <nicolasgiulixni.it@gmail.com>
2021-11-19 16:31:39 +0100

    s

---
+++ test_react.js
@@ -0,0 +1,14 @@
+class ShoppingList extends React.Component {
+  render() {
+    return (
+      <div className="shopping-list">
+        <h1>Lista della spesa per {this.props.name}</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
+    );
+  }
+}
